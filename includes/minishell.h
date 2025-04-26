a11dccb4ac9350c8f9eda284cda9be8263dab65f
Yassine Souaf <ysouaf@e3r2p18.1337.ma>
2025-04-26 13:25:38 +0100

    add new things

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,6 +2,15 @@
 #define MINISHELL_H
 
 
+typedef struct s_commands{
+    char **cmd;
+    char **inoutfile;
+    int fr_herdoc;
+    void *next;
+    void *prev;
+}   t_commands;
+
+
 typedef enum t_token_type{
     WORD, // string 
     HERDOC, // <<
@@ -15,7 +24,9 @@ typedef struct s_tokens{
     char *str;
     enum t_token_type type;
     s_tokens *next;
-} t_tokens;
+}   t_tokens;
+
+
 
 
 //FUNCTION EXECUTION
