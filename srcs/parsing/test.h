50f85187efc673fc6cf079b3a0acce856cdfab5f
Yassine Souaf <ysouaf@e3r4p13.1337.ma>
2025-04-26 18:10:44 +0100

    kantesti dak chi kanseparer pipes okda ka7thom fsplit diyalii test osf

---
+++ srcs/parsing/test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+
+char	**ft_split(char *s, char c);
+#endif
